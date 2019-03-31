ea66c0e4de3b63209a3567d6898995ad35d8f889
ciccig <cgerlitz08@gmail.com>
2019-03-31 21:45:34 +0200

    Fortsatt arbete med excerises

--- exercise2.py
+++ exercise2.py
@@ -29,9 +29,42 @@ NameSkillTown(name,town,skill)
 #     count = count +1
 
 #4
-inputWord = input("Enter a word: ")
+#inputWord = input("Enter a word: ")
 
-word = inputWord.upper()
-word2 = word.swapcase()
+#word = inputWord.upper()
+#print(word)
+#word2 = word[::-1]
 
-print(word2)
+#print(word2)
+
+#5 Simple Authenication
+import pandas as pd
+
+#Usernames and password
+df = {'Username': ["Agda", "Beata", "Celine", "Diana", "Eliza"], 'Password': ["111", "222", "333", "444","555"]}
+df = pd.DataFrame(data=df)
+df = df.set_index('Username')
+#Name and password for authentications
+user = input("Enter your username: ")
+
+userindex = df.index == user
+#Authentications
+c = 0
+for i in userindex:
+    if i:
+        passwordInput = input("Enter your password: ")
+        if passwordInput == df.loc[user,"Password"]:
+            print("authentication success")
+        else:
+            print("authentication failed")
+    c = c+1
+
+# 6 Hipp hipp hurray!
+for i in [1, 2, 3]:
+    for y in [1, 2]:
+        print("Hipp")
+    print("Hurray")
+
+# 7 Rovarspraket
+def rovarsprak(inputstring)
+    for 
